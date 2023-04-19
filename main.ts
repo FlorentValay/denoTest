90ce01174118e9133a69a26ed94d79d29521fee5
FlorentValay <florent.valay@gmail.com>
2023-04-19 17:36:35 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,25 @@
-import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
+const html = `
+<form method="POST" action="/">
+  <input type="text" name="name" placeholder="Your name">
+  <button type="submit">Submit</button>
+</form>
+`;
 
-serve((_req) => {
-  return new Response("Hello World!", {
-    headers: { "content-type": "text/plain" },
-  });
-});
+async function handler(req: Request): Promise<Response> {
+  switch (req.method) {
+    case "GET": {
+      return new Response(html, {
+        headers: { "content-type": "text/html; charset=utf-8" },
+      });
+    }
+
+    case "POST": {
+      const body = await req.formData();
+      const name = body.get("name") || "anonymous";
+      return new Response(`Hello ${name}!`);
+    }
+
+    default:
+      return new Response("Invalid method", { status: 405 });
+  }
+}
