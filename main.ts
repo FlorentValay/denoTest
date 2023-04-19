aa709e64df22d3359fa863f66483092485eac66c
FlorentValay <florent.valay@gmail.com>
2023-04-19 17:34:52 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
-function handler(_req: Request): Response {
-  return new Response("Hello, World");
-}
+import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
+
+serve((_req) => {
+  return new Response("Hello World!", {
+    headers: { "content-type": "text/plain" },
+  });
+});
